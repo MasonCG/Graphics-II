26cc1e34c0cd9c55da54d5f99b7ce20aad83fb34
Mason Goddard <107564580+MasonCG@users.noreply.github.com>
2025-03-17 09:37:17 -0400

    changes made

---
+++ BatchSquare.h
@@ -0,0 +1,18 @@
+//BlitSquare.h
+
+#pragma once
+#include "vkhelpers.h"
+#include "VertexManager.h"
+
+class DescriptorSet;
+class Image;
+
+class BlitSquare {
+public:
+    VertexManager::Info drawinfo;
+    BlitSquare(VertexManager* vertexManager);
+    void draw(VulkanContext* ctx, VkCommandBuffer cmd, DescriptorSet* descriptorSet, Image* img);
+
+    BlitSquare(const BlitSquare&) = delete;
+    void operator=(const BlitSquare&) = delete;
+}; 
